6ceb6bd03fede47735ed54e50a114e8b98e5fc95
Julio Rarick <juliorarick@gmail.com>
2024-07-15 20:39:50 -0300

    ✨ feat: sign up page

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import { router } from './routes'
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | mind.pizza" />
+      <Helmet titleTemplate="%s | mind.restaurants" />
       <Toaster richColors />
       <RouterProvider router={router} />
     </HelmetProvider>
